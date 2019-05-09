d68a245fcbcee04a03f37e2f91ad3f32c58ade72
Evgeny Shishko <ee.shishko@gmail.com>
2019-05-09 11:06:53 +0300

    changed on postgre

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'postgresql://127.0.0.1/music-synth'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'myverysecretlooooooooooongkey'
