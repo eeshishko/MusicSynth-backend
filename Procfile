4956eb73a7d9248b2866f536b8f71b8a0cd0cd7b
Evgeny Shishko <ee.shishko@gmail.com>
2019-05-12 15:56:00 +0300

    updated procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn music-synth:app
-worker: celery worker --app=tasks.celery
+worker: celery worker --app=app.celery
--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from celery import Celery
 
 
 def make_celery(flask_app):
-    print("CELERY_URL: ", flask_app.config['CELERY_RESULT_BACKEND'])
     celery = Celery(
         app.import_name,
         backend=flask_app.config['CELERY_RESULT_BACKEND'],
